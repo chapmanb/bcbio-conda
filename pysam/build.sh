b307e79f161def96d2b16e6fadcb7e8aa2a59902
chapmanb <chapmanb@50mail.com>
2014-08-05 21:56:11 -0400

    Bump pysam to 0.8.0 with fixes for handling SAM DS headers. Thanks to Severine Catreux

--- pysam/build.sh
+++ pysam/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Remove gcc statements that do not work on older compilers for CentOS5 support
+sed -i 's/"-Wno-error=declaration-after-statement",//g' setup.py
+sed -i 's/"-Wno-error=declaration-after-statement"//g' setup.py
 $PYTHON setup.py install
