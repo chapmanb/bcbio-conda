15d36c8e3bf8937bef3a03340b4307f7ab0df123
chapmanb <chapmanb@50mail.com>
2015-10-10 05:33:16 -0400

    pysam and pybedtools: transition to bioconda recipes

--- pysam/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Remove gcc statements that do not work on older compilers for CentOS5 support
-sed -i'' -e 's/"-Wno-error=declaration-after-statement",//g' setup.py
-sed -i'' -e 's/"-Wno-error=declaration-after-statement"//g' setup.py
-$PYTHON setup.py install
